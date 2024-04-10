76fe6ff2808543a52b0d3549ddf59afb1669061c
Felix Vazquez <flexiv@live.com>
2024-04-09 23:51:07 -0400

    tfcloud

--- providers.tf
+++ providers.tf
@@ -6,6 +6,13 @@ terraform {
       version = "~> 5.18"
     }
   }
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "FlexiV_Dev"
+    workspaces {
+      name = "flexiv-dev"
+    }
+  }
 }
 
 provider "github" {
